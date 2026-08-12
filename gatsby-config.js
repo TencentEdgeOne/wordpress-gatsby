2bc9f1ea3ed780fd29e3c26e8c4c7a215dda1af2
edgeone pages <simxin@tencent.com>
2026-08-12 19:54:44 +0800

    update meta info

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,9 @@ require('dotenv').config();
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    keywords: "EdgeOne Makers, Demo only",
+    title: `Gatsby Default Starter | EdgeOne Makers`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need. · Demo only · EdgeOne Makers`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
